0d73618bfd5afdba338d180a94fa73991a475492
ruslanzhumabek <j.ruslan@dogovor24.kz>
2022-04-14 12:29:49 +0600

    added new page

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import ("fmt"
         "net/http")
 
 func main() {
+  handleRequest()
+}
+
+func handleRequest() {
   http.HandleFunc("/", home_page)
+  http.HandleFunc("/contacts/", contacts_page)
   http.ListenAndServe(":8080", nil)
 }
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(page, "Home Page")
+func home_page(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Home Page")
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request)  {
+  fmt.Fprintf(w, "Contacts Page")
 }
