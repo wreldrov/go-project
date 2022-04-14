7bc54eb464c92f45667b611fa900bde0a75910d7
ruslanzhumabek <j.ruslan@dogovor24.kz>
2022-04-14 17:43:47 +0600

    working with html templates

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
-import ("fmt"
-        "net/http")
+import ("fmt"; "net/http"; "html/template")
 
 type User struct {
-  name string
-  age uint16
-  money int16
-  avg_grades, happiness float64
+  Name string
+  Age uint16
+  Money int16
+  Avg_grades, Happiness float64
+  Hobbies []string
 }
 
 func (u User) getAllInfo() string {
-  return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+  return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName (newName string) {
-  u.name = newName
+  u.Name = newName
 }
 
 func main() {
   // var bob User = ....
-  // bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
+  // bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 4.2, Happiness: 0.8}
 
   handleRequest()
 }
@@ -32,10 +32,11 @@ func handleRequest() {
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-  bob := User{"Bob", 25, -50, 4.2, 0.8}
-  bob.setNewName("Alex")
-  fmt.Fprintf(w, "Home Page\n")
-  fmt.Fprintf(w, bob.getAllInfo())
+  bob := User{"Bob", 25, 50, 4.2, 0.8, []string{"Football", "Tennis", "Bord"}}
+  // fmt.Fprintf(w, `<h1>Home Page</h1>
+  //   <b>Main Text</b>`)
+  tmpl, _ := template.ParseFiles("templates/home_page.html")
+  tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request)  {
