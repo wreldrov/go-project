50ce06824779511b44ea56be1eff44089e072779
ruslanzhumabek <j.ruslan@dogovor24.kz>
2022-04-16 15:49:37 +0600

    SQL Commands

--- main-sql.go
+++ main-sql.go
@@ -7,6 +7,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+  Id uint16 `json:"id"`
+  Name string `json:"name"`
+  Age uint16 `json:"age"`
+}
+
 func main() {
   fmt.Println("Work with MySQL")
 
@@ -16,6 +22,31 @@ func main() {
   }
 
   defer db.Close()
+
+  // Установка данных
+  // insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES ('Bob', 35)")
+  // if (err != nil) {
+  //   panic(err)
+  // }
+  //
+  // defer insert.Close()
+
+
+  // Выборка данных
+  res, err := db.Query("SELECT * FROM `users`")
+  if (err != nil) {
+    panic(err)
+  }
+
+  for res.Next() {
+    var user User
+    err = res.Scan(&user.Id, &user.Name, &user.Age)
+    if err != nil {
+      panic(err)
+    }
+
+    fmt.Println(fmt.Sprintf("%d. User: %s with age %d", user.Id, user.Name, user.Age))
+  }
 
   fmt.Println("Connected to MySQL")
 }
